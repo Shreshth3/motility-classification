58fca55f032454f5802eaadc0176c42ce6b45bc2
rchalamala <rchalamala@outlook.com>
2023-02-10 16:25:35 -0800

    basic model working too high of accuracy rn

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+import pandas as pd
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -16,7 +18,20 @@ from datetime  import datetime
 import csv
 import json
 
+def df_to_tensor(df):
+    return torch.Tensor(df.to_numpy())
+
+def process_data(csv):
+    df = pd.read_csv(csv)
+
+    df.dropna(inplace=True)
+
+    Y = df['label'].copy()
+
+    df.drop(columns=['uid', 'label'], inplace=True)
 
 
+    return df_to_tensor(df), torch.FloatTensor(Y.to_numpy())
 
-# insert model here lmfao
+
+    
